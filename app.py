981432991f1c833947bc2f52f063ab8e8718f78e
guilherme <guiurbano11@gmail.com>
2026-04-24 01:44:44 +0100

    login/senha

--- app.py
+++ app.py
@@ -55,4 +55,5 @@ def criar_app(config_name: str = None) -> Flask:
 app = criar_app()
 
 if __name__ == '__main__':
-    app.run(debug=app.config['DEBUG'], host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=app.config['DEBUG'], host='0.0.0.0', port=port)
